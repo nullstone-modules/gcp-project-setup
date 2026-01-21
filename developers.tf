0567aed5467cf0a362da597401e72150592c1432
Brad Sickles <brad.sickles@gmail.com>
2026-01-21 15:37:57 -0500

    developer access does not override other access

--- developers.tf
+++ developers.tf
@@ -2,66 +2,66 @@
 # Developers: Configure access to a group of developers
 # -------------------------------------------------------------------
 
-resource "google_project_iam_binding" "developers_logging_viewer" {
+resource "google_project_iam_member" "developers_logging_viewer" {
   count = var.developers_group != "" ? 1 : 0
 
   project = local.project_id
   role    = "roles/logging.viewer"
-  members = ["group:${var.developers_group}"]
+  member = "group:${var.developers_group}"
 }
 
-resource "google_project_iam_binding" "developers_monitoring_viewer" {
+resource "google_project_iam_member" "developers_monitoring_viewer" {
   count = var.developers_group != "" ? 1 : 0
 
   project = local.project_id
   role    = "roles/monitoring.viewer"
-  members = ["group:${var.developers_group}"]
+  member = "group:${var.developers_group}"
 }
 
-resource "google_project_iam_binding" "developers_run_developer" {
+resource "google_project_iam_member" "developers_run_developer" {
   count = var.developers_group != "" ? 1 : 0
 
   project = local.project_id
   role    = "roles/run.developer"
-  members = ["group:${var.developers_group}"]
+  member = "group:${var.developers_group}"
 }
 
-resource "google_project_iam_binding" "developers_k8s_developer" {
+resource "google_project_iam_member" "developers_k8s_developer" {
   count = var.developers_group != "" ? 1 : 0
 
   project = local.project_id
   role    = "roles/container.developer"
-  members = ["group:${var.developers_group}"]
+  member = "group:${var.developers_group}"
 }
 
-resource "google_project_iam_binding" "developers_storage_object_admin" {
+resource "google_project_iam_member" "developers_storage_object_admin" {
   count = var.developers_group != "" ? 1 : 0
 
   project = local.project_id
   role    = "roles/storage.objectAdmin"
-  members = ["group:${var.developers_group}"]
+  member = "group:${var.developers_group}"
 }
 
-resource "google_project_iam_binding" "developers_storage_admin" {
+resource "google_project_iam_member" "developers_storage_admin" {
   count = var.developers_group != "" ? 1 : 0
 
   project = local.project_id
   role    = "roles/storage.admin"
-  members = ["group:${var.developers_group}"]
+  member = "group:${var.developers_group}"
 }
 
-resource "google_project_iam_binding" "developers_pubsub_admin" {
+resource "google_project_iam_member" "developers_pubsub_admin" {
   count = var.developers_group != "" ? 1 : 0
 
   project = local.project_id
   role    = "roles/pubsub.admin"
-  members = ["group:${var.developers_group}"]
+  member = "group:${var.developers_group}"
 }
 
-resource "google_project_iam_binding" "developers_workflows_admin" {
+resource "google_project_iam_member" "developers_workflows_admin" {
   count = var.developers_group != "" ? 1 : 0
 
   project = local.project_id
   role    = "roles/workflows.admin"
-  members = ["group:${var.developers_group}"]
+  member = "group:${var.developers_group}"
 }
