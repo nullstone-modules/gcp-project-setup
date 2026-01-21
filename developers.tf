7231ed121cb8895a87dda39bfcea897526f12555
Brad Sickles <brad.sickles@gmail.com>
2026-01-21 15:29:59 -0500

    Added additional permissions to developers

--- developers.tf
+++ developers.tf
@@ -23,7 +23,15 @@ resource "google_project_iam_binding" "developers_run_developer" {
 
   project = local.project_id
   role    = "roles/run.developer"
-  members = ["group:${var.developers_group}", ]
+  members = ["group:${var.developers_group}"]
+}
+
+resource "google_project_iam_binding" "developers_k8s_developer" {
+  count = var.developers_group != "" ? 1 : 0
+
+  project = local.project_id
+  role    = "roles/container.developer"
+  members = ["group:${var.developers_group}"]
 }
 
 resource "google_project_iam_binding" "developers_storage_object_admin" {
@@ -31,5 +39,29 @@ resource "google_project_iam_binding" "developers_storage_object_admin" {
 
   project = local.project_id
   role    = "roles/storage.objectAdmin"
-  members = ["group:${var.developers_group}", ]
+  members = ["group:${var.developers_group}"]
+}
+
+resource "google_project_iam_binding" "developers_storage_admin" {
+  count = var.developers_group != "" ? 1 : 0
+
+  project = local.project_id
+  role    = "roles/storage.admin"
+  members = ["group:${var.developers_group}"]
+}
+
+resource "google_project_iam_binding" "developers_pubsub_admin" {
+  count = var.developers_group != "" ? 1 : 0
+
+  project = local.project_id
+  role    = "roles/pubsub.admin"
+  members = ["group:${var.developers_group}"]
+}
+
+resource "google_project_iam_binding" "developers_workflows_admin" {
+  count = var.developers_group != "" ? 1 : 0
+
+  project = local.project_id
+  role    = "roles/workflows.admin"
+  members = ["group:${var.developers_group}"]
 }
